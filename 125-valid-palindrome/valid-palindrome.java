361a96bfab66e6f216487f134353a5281437a814
Ragib Gulzar <ragibgulzar@gmail.com>
2026-07-22 17:58:09 +0530

    Time: 15 ms (20.18%) | Memory: 46.1 MB (33.85%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int low = 0;
+        int high = s.length()-1;
+        while(low<high){
+            if(s.charAt(low)!=s.charAt(high)){
+                return false;
+            }
+            low++;
+            high--;
+        }
+
+        return true;
+    }
+}
