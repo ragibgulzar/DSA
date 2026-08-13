80e246b9d13cb7b12821a364cf917bf0a435623a
Ragib Gulzar <ragibgulzar@gmail.com>
2026-08-14 00:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (88.12%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -3,18 +3,16 @@ class Solution {
         int left = 0;
         int right = letters.length-1;
 
-        while(left <= right ){
-            int mid = left + ( right - left ) / 2;
+        while(left<=right){
+            int mid = left + (right - left) / 2;
 
-            
-            if (target < letters[mid]){
-                right = mid - 1;
-            }
-            else {
-                
-                left = mid + 1;
-            }
+            if(letters[mid]>target){
+                right = mid -1;
+            } else if(letters[mid]<=target){
+                 left = mid+1;
+            } 
         }
         return letters[left%letters.length];
+
     }
 }
