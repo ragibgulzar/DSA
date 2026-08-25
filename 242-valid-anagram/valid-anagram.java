f8fb344f9c15dc0aeae82f2512176af785c1ac6a
Ragib Gulzar <ragibgulzar@gmail.com>
2026-08-25 08:29:41 +0530

    Time: 5 ms (62.25%) | Memory: 44.3 MB (82.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,16 +3,20 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-       int[] freq = new int[26];
+        int[] freq = new int[26];
         for(int i = 0; i < s.length(); i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
-        }
-        for(int x: freq){
-            if(x!=0) return false;
+            char x = s.charAt(i);
+            char y = t.charAt(i);
+
+            freq[x-'a']++;
+            freq[y-'a']--;
         }
 
-
+        for(int c: freq){
+            if(c!=0){
+                return false;
+            }
+        }
         return true;
     }
 }
