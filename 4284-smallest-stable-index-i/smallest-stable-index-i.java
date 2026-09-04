bdf50fe643b949fa6d02d73bb4e5edf3d7d63d8c
Ragib Gulzar <ragibgulzar@gmail.com>
2026-09-04 08:04:15 +0530

    Time: 1 ms (99.79%) | Memory: 46.3 MB (49.36%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -1,17 +1,15 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-         int[] max = new int[nums.length];
-         int[] min = new int[nums.length];
-         max[0]= nums[0];
-         for(int i = 1; i < max.length; i++){
-            max[i] = Math.max(nums[i],max[i-1]);
+         int[] suffixMin = new int[nums.length];
+         suffixMin[suffixMin.length-1]= nums[nums.length-1];
+         for(int i = suffixMin.length-2; i >= 0; i--){
+            suffixMin[i] = Math.min(nums[i],suffixMin[i+1]);
          }
-         min[min.length-1]= nums[nums.length-1];
-         for(int i = min.length-2; i >= 0; i--){
-            min[i] = Math.min(nums[i],min[i+1]);
-         }
+         int prefixMax = nums[0];
+         
         for(int i = 0; i < nums.length; i++){
-            if(max[i] - min[i] <= k ){
+            prefixMax = Math.max(prefixMax, nums[i]);
+            if(prefixMax - suffixMin[i] <= k ){
                 return i;
             }
         }
