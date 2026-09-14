8f36e4d70bbbc649d6afa1b7ccf0c70b943db6e9
Ragib Gulzar <ragibgulzar@gmail.com>
2026-09-15 00:48:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (19.87%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -17,24 +17,13 @@ class Solution {
             if(list1.val<list2.val){
                 temp.next = list1;
                 list1 = list1.next;
-                temp = temp.next;
             }else{
                 temp.next = list2;
                 list2 = list2.next;
-                temp = temp.next;
             }
-        }
-        while(list1!=null){
-            temp.next = list1;
             temp = temp.next;
-            list1 = list1.next;
         }
-
-        while(list2!=null){
-            temp.next = list2;
-            temp = temp.next;
-            list2 = list2.next;
-        }
+        temp.next = list1 != null ? list1 : list2;
 
         return list.next;
     }
